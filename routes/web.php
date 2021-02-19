e81181a0546ba74db740f13238b59654b45bf842
Cristovão Jobs <minguibernardo@gmail.com>
2021-02-19 08:15:12 +0100

    all
    all

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,9 @@ use App\Http\Controllers\{ //fazendo uma declaração importação varios contro
 
 //Rotas do type get
 
-Route::get('/post', [PostController::class, 'list'])->name('post.list');   //para listar usamos get
-Route::get('/post/add', [PostController::class, 'add'])->name('post.add'); //enviar dados no banco dedados
-Route::get('/post/about', [PostController::class, 'about'])->name('post.about');
+Route::get('/post', [PostController::class, 'list'])->middleware(['auth'])->name('post.list');   //para listar usamos get
+Route::get('/post/add', [PostController::class, 'add'])->middleware(['auth'])->name('post.add'); //enviar dados no banco dedados
+Route::get('/post/about', [PostController::class, 'about'])->middleware(['auth'])->name('post.about');
 
 
 //Rota da home principal
@@ -22,19 +22,19 @@ Route::get('/', function () {
 
 
 //Route type post
-Route::post('/post', [PostController::class, 'store'])->name('post.store'); // rota para o method action  do nosso formulario fazer o cadastro na BD
-Route::any('/post/search', [PostController::class, 'search'])->name('post.search'); // routa para pesquisa
+Route::post('/post', [PostController::class, 'store'])->middleware(['auth'])->name('post.store'); // rota para o method action  do nosso formulario fazer o cadastro na BD
+Route::any('/post/search', [PostController::class, 'search'])->middleware(['auth'])->name('post.search'); // routa para pesquisa
 //pegando dado na url
-Route::get('/post/{id}', [PostController::class, 'show'])->name('post.show');
+Route::get('/post/{id}', [PostController::class, 'show'])->middleware(['auth'])->name('post.show');
 
 //rota para a  pagina atualizar
-Route::get('/post/edit/{id}', [PostController::class, 'edit'])->name('post.edit');
+Route::get('/post/edit/{id}', [PostController::class, 'edit'])->middleware(['auth'])->name('post.edit');
 
 //routa para edicao de registro
-Route::put('/post/{id}', [PostController::class, 'update'])->name('post.update');
+Route::put('/post/{id}', [PostController::class, 'update'])->middleware(['auth'])->name('post.update');
 
 //deletando um item com http delete
-Route::delete('/post/{id}', [PostController::class, 'deleted'])->name('post.deleted');
+Route::delete('/post/{id}', [PostController::class, 'deleted'])->middleware(['auth'])->name('post.deleted');
 
 ///routa para o painel de control
 Route::get('/dashboard', function () {  return view('dashboard');})->middleware(['auth'])->name('dashboard');
